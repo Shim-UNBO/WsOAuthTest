603ceeb419b79a043523ad5b04b80923bf0628a7
shimUnBo <pencer118@gmail.com>
2024-04-29 23:59:11 +0900

    kakao test

--- src/main/java/com/example/demo/config/RestTemplateConfig.java
+++ src/main/java/com/example/demo/config/RestTemplateConfig.java
@@ -1,32 +1,26 @@
-//package com.example.demo.config;
-//
-//import org.apache.hc.client5.http.config.RequestConfig;
-//import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-//import org.apache.hc.client5.http.impl.classic.HttpClients;
-//import org.apache.hc.core5.http.HttpHost;
-//import org.apache.hc.core5.util.TimeValue;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-//import org.springframework.web.client.RestTemplate;
-//
-//@Configuration
-//public class RestTemplateConfig {
-//
-//    @Bean(name = "proxyRestTemplate")
-//    public RestTemplate restTemplate() {
-//        HttpHost proxy = new HttpHost("krmp-proxy.9rum.cc", 3128);
-//        RequestConfig config = RequestConfig.custom()
-//                .setConnectTimeout(TimeValue.ofMilliseconds(5000).toTimeout())
-//                .setResponseTimeout(TimeValue.ofMilliseconds(10000).toTimeout())
-//                .build();
-//
-//        CloseableHttpClient httpClient = HttpClients.custom()
-//                .setProxy(proxy)
-//                .setDefaultRequestConfig(config)
-//                .build();
-//
-//        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-//        return new RestTemplate(factory);
-//    }
-//}
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.http.HttpHost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+
+    @Bean(name = "proxyRestTemplate")
+    public RestTemplate restTemplate() {
+        HttpHost proxy = new HttpHost("krmp-proxy.9rum.cc", 3128);
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setProxy(proxy)
+                .build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(10000);
+        return new RestTemplate(factory);
+    }
+}
