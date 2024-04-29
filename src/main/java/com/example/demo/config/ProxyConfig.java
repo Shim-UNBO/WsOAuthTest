532b54e0aec85e1a6cee53caebfbc4614c943c8d
shimUnBo <pencer118@gmail.com>
2024-04-29 22:34:18 +0900

    kakao test

---
+++ src/main/java/com/example/demo/config/ProxyConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
+@Configuration
+public class ProxyConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("krmp-proxy.9rum.cc", 3128));
+        requestFactory.setProxy(proxy);
+        restTemplate.setRequestFactory(requestFactory);
+
+        return restTemplate;
+    }
+}
